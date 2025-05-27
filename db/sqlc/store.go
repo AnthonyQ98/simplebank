887ca564f94f361521133706492cd87fb828a43d
Anthony Quinn <anthonyquinn.developer@gmail.com>
2025-05-27 23:07:17 +0100

    Handling transaction locks & deadlock scenarios

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -81,7 +81,18 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		// Update accounts' balance
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountID,
+			Amount: arg.Amount,
+		})
 		return nil
 	})
 
