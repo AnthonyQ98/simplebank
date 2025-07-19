8fa9fad96d69d7afbba83e21b286bf0ec1113ff2
Anthony Quinn <anthonyquinn.developer@gmail.com>
2025-07-19 21:37:57 +0100

    update gitignore

---
+++ terraform/eks/simplebank_eks.tf
@@ -0,0 +1,63 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_iam_role" "cluster-role" {
+  name = "simplebank-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-role-eks-policy" {
+  role       = aws_iam_role.cluster-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
+
+  cluster_name    = "simple-bank"
+  cluster_version = "1.31"
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+  iam_role_arn = aws_iam_role.cluster-role.arn
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t2.micro"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+    }
+  }
+
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
