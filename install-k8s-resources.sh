ee6d7d11f1a229964588d5208f6ebecc3d7b444c
Anthony Quinn <anthonyquinn.developer@gmail.com>
2025-07-20 22:21:26 +0100

    Add TLS

---
+++ install-k8s-resources.sh
@@ -0,0 +1,5 @@
+kubectl apply -f k8s-resources/
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.13.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.18.2/cert-manager.yaml
+
+# other required steps: get the address from the ingress created when the above is applied & update Route 53 accordingly. As I destroy all AWS resources between each development session for cost saving purposes, these manual steps are partially required.
