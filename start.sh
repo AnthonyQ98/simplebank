fa7ca9cff0fc92ba501de3eb042852844185f0f4
Anthony Quinn <anthonyquinn.developer@gmail.com>
2025-07-27 17:35:27 +0100

    test fix attempt

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-set -e 
-
-echo "run db migrations"
-echo "DB_SOURCE is: $DB_SOURCE"
-source /app/app.env
-echo "DB_SOURCE is: $DB_SOURCE"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+set -e
 
 echo "start the app"
-
 exec "$@"
