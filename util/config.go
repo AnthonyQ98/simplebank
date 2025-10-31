3f874c15ba1514ce2866fee0c2439a77585e9bc7
Anthony Quinn <anthonyquinn.developer@gmail.com>
2025-10-31 23:10:23 +0000

    another attempt to fix github workflow tests

--- util/config.go
+++ util/config.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"strings"
 	"time"
 
 	"github.com/spf13/viper"
@@ -27,7 +28,24 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
 
+	// Explicitly bind environment variables to ensure they work even without config file
 	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	// Bind each environment variable explicitly
+	viper.BindEnv("ENVIRONMENT")
+	viper.BindEnv("DB_DRIVER")
+	viper.BindEnv("DB_SOURCE")
+	viper.BindEnv("MIGRATION_URL")
+	viper.BindEnv("HTTP_SERVER_ADDRESS")
+	viper.BindEnv("GRPC_SERVER_ADDRESS")
+	viper.BindEnv("TOKEN_SYMMETRIC_KEY")
+	viper.BindEnv("ACCESS_TOKEN_DURATION")
+	viper.BindEnv("REFRESH_TOKEN_DURATION")
+	viper.BindEnv("REDIS_ADDRESS")
+	viper.BindEnv("EMAIL_SENDER_NAME")
+	viper.BindEnv("EMAIL_SENDER_ADDRESS")
+	viper.BindEnv("EMAIL_SENDER_PASSWORD")
 
 	// Try to read config file, but don't fail if it doesn't exist
 	// This allows the function to work with environment variables only
